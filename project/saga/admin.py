a03278d7e1750339e998612ff4e4439414707f51
perspace42 <108037333+perspace42@users.noreply.github.com>
2024-06-11 15:21:29 -0700

    fix admin route

--- project/saga/admin.py
+++ project/saga/admin.py
@@ -9,6 +9,20 @@ Configure the administrator access to the database data, and the UI for acessing
 from django.contrib import admin
 from .models import Unit,Faction
 
-# Register your models here.
+class UnitInline(admin.TabularInline):
+    model = Unit
+    extra = 0
+
+class FactionAdmin(admin.ModelAdmin):
+    inlines = [UnitInline]
+    list_display = ('name','description','specialRules')
+
+#Register Your Models Here
+admin.site.register(Faction,FactionAdmin)
+
+
+'''    
+# Old way to register models
 admin.site.register(Faction)
 admin.site.register(Unit)
+'''
